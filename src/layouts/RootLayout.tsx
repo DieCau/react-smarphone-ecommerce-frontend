fba635f4acfa7b9c114ab298292cb635b78c41ed
Diego Caucota <diegocaucota@gmail.com>
2025-06-12 01:30:58 -0300

    add: layouts DashboardLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,17 +1,37 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Navbar } from '../components/shared/Navbar';
 import { Footer } from '../components/shared/Footer';
+import { Banner } from '../components/home/Banner';
+import { Newsletter } from '../components/home/Newsletter';
+import { Sheet } from '../components/shared/Sheet';
+import { useGlobalStore } from '../store/global.store';
+import { NavbarMobile } from '../components/shared/NavbarMobile';
 
 export const RootLayout = () => {
-  return (
-    <div className="h-screen flex flex-col">
-      <Navbar />
+	const { pathname } = useLocation();
 
-      <main className="container my-8 flex-1">
-        <Outlet />
-      </main>
+	const isSheetOpen = useGlobalStore(state => state.isSheetOpen);
+	const activeNavMobile = useGlobalStore(
+		state => state.activeNavMobile
+	);
 
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='h-screen flex flex-col font-montserrat'>
+			<Navbar />
+
+			{pathname === '/' && <Banner />}
+
+			<main className='container my-8 flex-1'>
+				<Outlet />
+			</main>
+
+			{pathname === '/' && <Newsletter />}
+
+			{isSheetOpen && <Sheet />}
+
+			{activeNavMobile && <NavbarMobile />}
+
+			<Footer />
+		</div>
+	);
 };
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,22 +1,113 @@
-import { createBrowserRouter } from "react-router-dom";
-import { RootLayout } from "../layouts/RootLayout";
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { RootLayout } from '../layouts/RootLayout';
+import {
+  HomePage,
+  CellPhonesPage,
+  AboutPage,
+  CellPhonePage,
+  LoginPage,
+  RegisterPage,
+  OrdersUserPage,
+  CheckoutPage,
+  ThankyouPage,
+  OrderUserPage,
+} from '../pages';
+
+import {
+  DashboardProductsPage,
+  DashboardNewProductPage,
+  DashboardProductSlugPage,
+  DashboardOrdersPage,
+  DashboardOrderPage,
+} from '../pages/dashboard';
+
+import { ClientLayout } from '../layouts/ClientLayout';
+import { DashboardLayout } from '../layouts/DashboardLayout';
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     children: [
       {
         index: true,
-        element: <h1>Inicio</h1>,
+        element: <HomePage />,
       },
       {
-        path: "celulares",
-        element: <h1>Celulares</h1>,
+        path: 'celulares',
+        element: <CellPhonesPage />,
       },
       {
-        path: "nosotros",
-        element: <h1>Sobre Nosotros</h1>,
+        path: 'celulares/:slug',
+        element: <CellPhonePage />,
+      },
+      {
+        path: 'nosotros',
+        element: <AboutPage />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'registro',
+        element: <RegisterPage />,
+      },
+      {
+        path: 'account',
+        element: <ClientLayout />,
+        children: [
+          {
+            path: '',
+            element: <Navigate to="/account/pedidos" />,
+          },
+          {
+            path: 'pedidos',
+            element: <OrdersUserPage />,
+          },
+          {
+            path: 'pedidos/:id',
+            element: <OrderUserPage />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: '/checkout',
+    element: <CheckoutPage />,
+  },
+  {
+    path: '/checkout/:id/thank-you',
+    element: <ThankyouPage />,
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/dashboard/productos" />,
+      },
+      {
+        path: 'productos',
+        element: <DashboardProductsPage />,
+      },
+      {
+        path: 'productos/new',
+        element: <DashboardNewProductPage />,
+      },
+      {
+        path: 'productos/editar/:slug',
+        element: <DashboardProductSlugPage />,
+      },
+      {
+        path: 'ordenes',
+        element: <DashboardOrdersPage />,
+      },
+      {
+        path: 'ordenes/:id',
+        element: <DashboardOrderPage />,
       },
     ],
   },
