4c3b01a39cf9063ebb409363040b3bcf70c1db9b
Diego Caucota <diegocaucota@gmail.com>
2025-06-16 23:55:46 -0300

    fix: orders TableOrdersAdmin

--- src/components/one-product/ProductDescription.tsx
+++ src/components/one-product/ProductDescription.tsx
@@ -3,27 +3,27 @@ import StarterKit from '@tiptap/starter-kit';
 import type { Json } from '../../supabase/supabase';
 
 interface Props {
-	content: JSONContent | Json;
+  content: JSONContent | Json;
 }
 
 export const ProductDescription = ({ content }: Props) => {
-	const editor = useEditor({
-		extensions: [StarterKit],
-		content: content as JSONContent,
-		editable: false,
-		editorProps: {
-			attributes: {
-				class: 'prose prose-sm sm:prose-base max-w-none',
-			},
-		},
-	});
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: content as JSONContent,
+    editable: false,
+    editorProps: {
+      attributes: {
+        class: 'prose prose-sm sm:prose-base max-w-none',
+      },
+    },
+  });
 
-	return (
-		<div className='mt-12'>
-			<h2 className='text-2xl font-bold text-center mb-8 underline'>
-				Descripción
-			</h2>
-			<EditorContent editor={editor} />
-		</div>
-	);
+  return (
+    <div className="mt-12">
+      <h2 className="text-2xl font-bold text-center mb-8 underline">
+        Descripción
+      </h2>
+      <EditorContent editor={editor} />
+    </div>
+  );
 };
