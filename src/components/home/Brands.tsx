6f991e0b903c86302e40b3950c54a5f61519e04e
Diego Caucota <diegocaucota@gmail.com>
2025-06-16 23:53:11 -0300

    fix: home Brands

--- src/components/home/Brands.tsx
+++ src/components/home/Brands.tsx
@@ -27,22 +27,22 @@ const brands = [
 ];
 
 export const Brands = () => {
-	return (
-		<div className='flex flex-col items-center gap-3 pt-16 pb-12'>
-			<h2 className='font-bold text-2xl'>Marcas que disponemos</h2>
+  return (
+    <div className="flex flex-col items-center gap-3 pt-16 pb-12">
+      <h2 className="font-bold text-2xl">Marcas que disponemos</h2>
 
-			<p className='w-2/3 text-center text-sm md:text-base'>
-				Tenemos lo más moderno en tecnología y los últimos modelos de
-				celulares disponibles
-			</p>
+      <p className="w-2/3 text-center text-sm md:text-base">
+        Tenemos lo más moderno en tecnología y los últimos modelos de celulares
+        disponibles
+      </p>
 
-			<div className='grid grid-cols-3 gap-6 mt-8 items-center md:grid-cols-6'>
-				{brands.map((brand, index) => (
-					<div key={index}>
-						<img src={brand.image} alt={brand.alt} />
-					</div>
-				))}
-			</div>
-		</div>
-	);
+      <div className="grid grid-cols-3 gap-6 mt-8 items-center md:grid-cols-6">
+        {brands.map((brand, index) => (
+          <div key={index}>
+            <img src={brand.image} alt={brand.alt} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
