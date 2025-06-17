b20be962c2c34b26e13dc38b27249e42b2cb35f6
Diego Caucota <diegocaucota@gmail.com>
2025-06-17 00:37:55 -0300

    fix: layouts ClientLayout

--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -7,61 +7,58 @@ import { Loader } from '../components/shared/Loader';
 import { HiOutlineExternalLink } from 'react-icons/hi';
 
 export const ClientLayout = () => {
-	const { session, isLoading: isLoadingSession } = useUser();
-	const { data: role, isLoading: isLoadingRole } = useRoleUser(
-		session?.user.id as string
-	);
+  const { session, isLoading: isLoadingSession } = useUser();
+  const { data: role, isLoading: isLoadingRole } = useRoleUser(
+    session?.user.id as string
+  );
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		supabase.auth.onAuthStateChange(async (event, session) => {
-			if (event === 'SIGNED_OUT' || !session) {
-				navigate('/login', { replace: true });
-			}
-		});
-	}, [navigate]);
+  useEffect(() => {
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event === 'SIGNED_OUT' || !session) {
+        navigate('/login', { replace: true });
+      }
+    });
+  }, [navigate]);
 
-	if (isLoadingSession || isLoadingRole) return <Loader />;
+  if (isLoadingSession || isLoadingRole) return <Loader />;
 
-	const handleLogout = async () => {
-		await signOut();
-	};
+  const handleLogout = async () => {
+    await signOut();
+  };
 
-	return (
-		<div className='flex flex-col gap-5'>
-			{/* Menú */}
-			<nav className='flex justify-center gap-10 text-sm font-medium'>
-				<NavLink
-					to='/account/pedidos'
-					className={({ isActive }) =>
-						`${isActive ? 'underline' : 'hover:underline'}`
-					}
-				>
-					Pedidos
-				</NavLink>
+  return (
+    <div className="flex flex-col gap-5">
+      {/* Menú */}
+      <nav className="flex justify-center gap-10 text-sm font-medium">
+        <NavLink
+          to="/account/pedidos"
+          className={({ isActive }) =>
+            `${isActive ? 'underline' : 'hover:underline'}`
+          }
+        >
+          Pedidos
+        </NavLink>
 
-				{role === 'admin' && (
-					<NavLink
-						to='/dashboard/productos'
-						className='flex items-center gap-1 hover:underline'
-					>
-						Dashboard
-						<HiOutlineExternalLink
-							size={16}
-							className='inline-block'
-						/>
-					</NavLink>
-				)}
+        {role === 'admin' && (
+          <NavLink
+            to="/dashboard/productos"
+            className="flex items-center gap-1 hover:underline"
+          >
+            Dashboard
+            <HiOutlineExternalLink size={16} className="inline-block" />
+          </NavLink>
+        )}
 
-				<button className='hover:underline' onClick={handleLogout}>
-					Cerrar sesión
-				</button>
-			</nav>
+        <button className="hover:underline" onClick={handleLogout}>
+          Cerrar sesión
+        </button>
+      </nav>
 
-			<main className='container mt-12 flex-1'>
-				<Outlet />
-			</main>
-		</div>
-	);
+      <main className="container mt-12 flex-1">
+        <Outlet />
+      </main>
+    </div>
+  );
 };
