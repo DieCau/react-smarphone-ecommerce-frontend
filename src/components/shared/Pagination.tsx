68949f4b8fcd2d197908fe0820b082c8e916f50b
Diego Caucota <diegocaucota@gmail.com>
2025-06-18 12:21:30 -0300

    fix: components shared Pagination

--- src/components/shared/Pagination.tsx
+++ src/components/shared/Pagination.tsx
@@ -1,54 +1,52 @@
 interface Props {
-	totalItems: number;
-	page: number;
-	setPage: React.Dispatch<React.SetStateAction<number>>;
+  totalItems: number;
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 export const Pagination = ({ totalItems, page, setPage }: Props) => {
-	const handleNextPage = () => {
-		setPage(page + 1);
-	};
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
 
-	const handlePrevPage = () => {
-		setPage(prevPage => Math.max(prevPage - 1, 1));
-	};
+  const handlePrevPage = () => {
+    setPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
 
-	const itemsPerPage = 10;
-	const totalPages = totalItems
-		? Math.ceil(totalItems / itemsPerPage)
-		: 1;
-	const isLastPage = page >= totalPages;
+  const itemsPerPage = 10;
+  const totalPages = totalItems ? Math.ceil(totalItems / itemsPerPage) : 1;
+  const isLastPage = page >= totalPages;
 
-	const startItem = (page - 1) * itemsPerPage + 1; // 1 -> 11 -> 21
-	const endItem = Math.min(page * itemsPerPage, totalItems);
+  const startItem = (page - 1) * itemsPerPage + 1; // 1 -> 11 -> 21
+  const endItem = Math.min(page * itemsPerPage, totalItems);
 
-	return (
-		<div className='flex justify-between items-center'>
-			<p className='text-xs font-medium'>
-				Mostrando{' '}
-				<span className='font-bold'>
-					{startItem} - {endItem}
-				</span>{' '}
-				de <span className='font-bold'> {totalItems}</span> productos
-			</p>
+  return (
+    <div className="flex justify-between items-center">
+      <p className="text-xs font-medium">
+        Mostrando{' '}
+        <span className="font-bold">
+          {startItem} - {endItem}
+        </span>{' '}
+        de <span className="font-bold"> {totalItems}</span> productos
+      </p>
 
-			<div className='flex gap-3'>
-				<button
-					className='btn-paginated'
-					onClick={handlePrevPage}
-					disabled={page === 1}
-				>
-					Anterior
-				</button>
+      <div className="flex gap-3">
+        <button
+          className="btn-paginated"
+          onClick={handlePrevPage}
+          disabled={page === 1}
+        >
+          Anterior
+        </button>
 
-				<button
-					className='btn-paginated'
-					onClick={handleNextPage}
-					disabled={isLastPage}
-				>
-					Siguiente
-				</button>
-			</div>
-		</div>
-	);
+        <button
+          className="btn-paginated"
+          onClick={handleNextPage}
+          disabled={isLastPage}
+        >
+          Siguiente
+        </button>
+      </div>
+    </div>
+  );
 };
