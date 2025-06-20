78a2b1c9aed16c08029599d60a56f717238a6f40
Diego Caucota <diegocaucota@gmail.com>
2025-06-20 00:20:34 -0300

    fix: hooks products useDeleteProduct

--- src/hooks/products/useDeleteProduct.ts
+++ src/hooks/products/useDeleteProduct.ts
@@ -3,28 +3,28 @@ import { deleteProduct } from '../../actions';
 import toast from 'react-hot-toast';
 
 export const useDeleteProduct = () => {
-	const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-	const { mutate, isPending } = useMutation({
-		mutationFn: deleteProduct,
-		onSuccess: () => {
-			queryClient.invalidateQueries({
-				queryKey: ['products'],
-			});
-			toast.success('Producto eliminado correctamente', {
-				position: 'bottom-right',
-			});
-		},
-		onError: error => {
-			console.log(error);
-			toast.error('Ocurrió un error al eliminar el producto', {
-				position: 'bottom-right',
-			});
-		},
-	});
+  const { mutate, isPending } = useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['products'],
+      });
+      toast.success('Producto eliminado correctamente', {
+        position: 'bottom-right',
+      });
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error('Ocurrió un error al eliminar el producto', {
+        position: 'bottom-right',
+      });
+    },
+  });
 
-	return {
-		mutate,
-		isPending,
-	};
+  return {
+    mutate,
+    isPending,
+  };
 };
