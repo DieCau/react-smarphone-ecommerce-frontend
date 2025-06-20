63d0709efd454d328b4c8d61766aa5e7f8a58b50
Diego Caucota <diegocaucota@gmail.com>
2025-06-20 00:23:52 -0300

    fix: hooks products useUpdateProduct

--- src/hooks/products/useUpdateProduct.ts
+++ src/hooks/products/useUpdateProduct.ts
@@ -5,31 +5,30 @@ import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
 export const useUpdateProduct = (productId: string) => {
-	const queryClient = useQueryClient();
-	const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
-	const { mutate, isPending } = useMutation({
-		mutationFn: async (data: ProductInput) =>
-			updateProduct(productId, data),
-		onSuccess: () => {
-			queryClient.invalidateQueries({
-				queryKey: ['products'],
-			});
-			toast.success('Producto actualizado', {
-				position: 'bottom-right',
-			});
-			navigate('/dashboard/productos');
-		},
-		onError: error => {
-			console.log(error);
-			toast.error('Ocurrió un error al actualizar el producto', {
-				position: 'bottom-right',
-			});
-		},
-	});
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (data: ProductInput) => updateProduct(productId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['products'],
+      });
+      toast.success('Producto actualizado', {
+        position: 'bottom-right',
+      });
+      navigate('/dashboard/productos');
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error('Ocurrió un error al actualizar el producto', {
+        position: 'bottom-right',
+      });
+    },
+  });
 
-	return {
-		mutate,
-		isPending,
-	};
+  return {
+    mutate,
+    isPending,
+  };
 };
