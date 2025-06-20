d5d2d38c71021abde0f8d8340ed40b130a2f3066
Diego Caucota <diegocaucota@gmail.com>
2025-06-19 23:14:56 -0300

    fix: hooks orders useOrderAdmin

--- src/hooks/orders/useOrderAdmin.ts
+++ src/hooks/orders/useOrderAdmin.ts
@@ -2,15 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { getOrderById } from '../../actions';
 
 export const useOrderAdmin = (id: number) => {
-	const { data, isLoading } = useQuery({
-		queryKey: ['order', 'admin', id],
-		queryFn: () => getOrderById(id),
-		enabled: !!id,
-		retry: false,
-	});
+  const { data, isLoading } = useQuery({
+    queryKey: ['order', 'admin', id],
+    queryFn: () => getOrderById(id),
+    enabled: !!id,
+    retry: false,
+  });
 
-	return {
-		data,
-		isLoading,
-	};
+  return {
+    data,
+    isLoading,
+  };
 };
