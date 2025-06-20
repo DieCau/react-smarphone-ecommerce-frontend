87176c6e8764c05b19edc1a7e23d8e88ec02dfcb
Diego Caucota <diegocaucota@gmail.com>
2025-06-20 00:19:03 -0300

    fix: hooks orders useOrders

--- src/hooks/orders/useOrders.ts
+++ src/hooks/orders/useOrders.ts
@@ -2,14 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import { getOrdersByCustomerId } from '../../actions';
 
 export const useOrders = () => {
-	const { data, isLoading } = useQuery({
-		queryKey: ['orders'],
-		queryFn: getOrdersByCustomerId,
-		retry: false,
-	});
+  const { data, isLoading } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrdersByCustomerId,
+    retry: false,
+  });
 
-	return {
-		data,
-		isLoading,
-	};
+  return {
+    data,
+    isLoading,
+  };
 };
