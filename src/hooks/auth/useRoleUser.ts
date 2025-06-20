d55bed9e00ad7f8bb8d2c635c442a0b4adc800af
Diego Caucota <diegocaucota@gmail.com>
2025-06-19 23:10:44 -0300

    fix: hooks auth useRoleUser

--- src/hooks/auth/useRoleUser.ts
+++ src/hooks/auth/useRoleUser.ts
@@ -2,14 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import { getUserRole } from '../../actions';
 
 export const useRoleUser = (userId: string) => {
-	const { data, isLoading } = useQuery({
-		queryKey: ['rol-user'],
-		queryFn: async () => await getUserRole(userId),
-		enabled: !!userId,
-	});
+  const { data, isLoading } = useQuery({
+    queryKey: ['rol-user'],
+    queryFn: async () => await getUserRole(userId),
+    enabled: !!userId,
+  });
 
-	return {
-		data,
-		isLoading,
-	};
+  return {
+    data,
+    isLoading,
+  };
 };
