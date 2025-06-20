c6462f40cc70ed1613a3e260490583beb9f82d88
Diego Caucota <diegocaucota@gmail.com>
2025-06-19 23:09:22 -0300

    fix: hooks auth useLogin

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -4,24 +4,24 @@ import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 export const useLogin = () => {
-	const navigate = useNavigate();
-	const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
-	const { mutate, isPending } = useMutation({
-		mutationFn: signIn,
-		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: ['user'] });
-			navigate('/');
-		},
-		onError: err => {
-			toast.error(err.message, {
-				position: 'bottom-right',
-			});
-		},
-	});
+  const { mutate, isPending } = useMutation({
+    mutationFn: signIn,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] });
+      navigate('/');
+    },
+    onError: (err) => {
+      toast.error(err.message, {
+        position: 'bottom-right',
+      });
+    },
+  });
 
-	return {
-		mutate,
-		isPending,
-	};
+  return {
+    mutate,
+    isPending,
+  };
 };
