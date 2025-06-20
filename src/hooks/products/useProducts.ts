b5a5be9130238c2ec894646db03aceffa40a4e74
Diego Caucota <diegocaucota@gmail.com>
2025-06-20 00:23:15 -0300

    fix: hooks products useProducts

--- src/hooks/products/useProducts.ts
+++ src/hooks/products/useProducts.ts
@@ -2,15 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { getProducts } from '../../actions';
 
 export const useProducts = ({ page = 1 }: { page?: number }) => {
-	const { data, isLoading } = useQuery({
-		queryKey: ['products', page],
-		queryFn: () => getProducts(page),
-		staleTime: 1000 * 60 * 5, // 1 hora
-	});
+  const { data, isLoading } = useQuery({
+    queryKey: ['products', page],
+    queryFn: () => getProducts(page),
+    staleTime: 1000 * 60 * 5, // 1 hora
+  });
 
-	return {
-		products: data?.products,
-		isLoading,
-		totalProducts: data?.count ?? 0,
-	};
+  return {
+    products: data?.products,
+    isLoading,
+    totalProducts: data?.count ?? 0,
+  };
 };
