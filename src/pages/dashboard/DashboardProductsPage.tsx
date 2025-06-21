6dbf45057aeeaf287a6b7fda91672ca79da9c00c
Diego Caucota <diegocaucota@gmail.com>
2025-06-21 20:07:19 -0300

    fix: pages dashboard DashboardProductsPage

--- src/pages/dashboard/DashboardProductsPage.tsx
+++ src/pages/dashboard/DashboardProductsPage.tsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 import { TableProduct } from '../../components/dashboard';
 
 export const DashboardProductsPage = () => {
-	return (
-		<div className='h-full flex flex-col gap-2'>
-			<Link
-				to='/dashboard/productos/new'
-				className='bg-black text-white flex items-center self-end py-[6px] px-2 rounded-md text-sm gap-1 font-semibold'
-			>
-				<IoAddCircleOutline className='inline-block' />
-				Nuevo Producto
-			</Link>
+  return (
+    <div className="h-full flex flex-col gap-2">
+      <Link
+        to="/dashboard/productos/new"
+        className="bg-black text-white flex items-center self-end py-[6px] px-2 rounded-md text-sm gap-1 font-semibold"
+      >
+        <IoAddCircleOutline className="inline-block" />
+        Nuevo Producto
+      </Link>
 
-            <TableProduct />
-		</div>
-	);
+      <TableProduct />
+    </div>
+  );
 };
