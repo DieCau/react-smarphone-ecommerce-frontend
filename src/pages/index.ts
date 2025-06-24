73c508dbd18e8d27895025d6e55d82f9cbd44f09
Diego Caucota <diegocaucota@gmail.com>
2025-06-23 23:47:37 -0300

    fix: dashboard index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,19 +1,19 @@
-export * from './HomePage';
-export * from './CellPhonesPage';
 export * from './AboutPage';
 export * from './CellPhonePage';
-export * from './LoginPage';
-export * from './RegisterPage';
-export * from './OrdersUserPage';
+export * from './CellPhonesPage';
 export * from './CheckoutPage';
-export * from './ThankyouPage';
+export * from './HomePage';
+export * from './LoginPage';
+export * from './OrdersUserPage';
 export * from './OrderUserPage';
+export * from './RegisterPage';
+export * from './ThankyouPage';
 
 /* ********************************** */
 /*           DASHBOARD PAGES          */
 /* ********************************** */
-export * from './dashboard/DashboardProductsPage';
 export * from './dashboard/DashboardNewProductPage';
 export * from './dashboard/DashboardProductSlugPage';
-export * from "./dashboard/DashboardOrdersPage"
+export * from './dashboard/DashboardProductsPage';
 export * from "./dashboard/DashboardOrderPage"
+export * from "./dashboard/DashboardOrdersPage"
