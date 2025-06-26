9c611bacc023e283938a26f029b0838c84060de2
Diego Caucota <diegocaucota@gmail.com>
2025-06-26 01:01:45 -0300

    fix: hooks auth useRegister documentation

--- src/hooks/auth/useRegister.ts
+++ src/hooks/auth/useRegister.ts
@@ -4,6 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 export const useRegister = () => {
+  // This hook is used to handle user registration.
+  // It uses the useMutation hook from react-query to perform the sign-up action.
+  // It also uses useNavigate from react-router-dom to redirect the user after successful registration.
+  // It also uses useQueryClient from react-query to invalidate the user query after successful registration.
+  // It returns an object with the mutate function and the isPending state.
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
