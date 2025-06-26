9516597415df32fe13f85c24f3e8ba0edf904d18
Diego Caucota <diegocaucota@gmail.com>
2025-06-26 01:00:06 -0300

    fix: hooks auth useCustomer documentation

--- src/hooks/auth/useCustomer.ts
+++ src/hooks/auth/useCustomer.ts
@@ -2,6 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { getUserData } from '../../actions';
 
 export const useCustomer = (userId: string) => {
+
+	// Fetch user data based on the provided userId
+	// This hook will return the user data and loading state
+	// It will only run if userId is truthy
+	// and will not retry on failure.
+	// It will refetch data when the window is focused.
+	// The query key is ['customer', userId] to uniquely identify the query.
+	// The query function is getUserData which fetches the user data from the server.
+	// The return value is an object containing the data and isLoading state.
 	const { data, isLoading } = useQuery({
 		queryKey: ['customer', userId],
 		queryFn: () => getUserData(userId),
