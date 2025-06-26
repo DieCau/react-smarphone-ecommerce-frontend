d78f33e30c29eff4d9cec7591806e06e115d5af9
Diego Caucota <diegocaucota@gmail.com>
2025-06-26 01:03:38 -0300

    fix: hooks auth useUser documentation

--- src/hooks/auth/useUser.ts
+++ src/hooks/auth/useUser.ts
@@ -2,6 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { getSession } from '../../actions';
 
 export const useUser = () => {
+  // This hook is used to fetch the current user's session data.
+  // It uses the `useQuery` hook from `@tanstack/react-query` to
+  // perform the getSession action and manage the loading state.
+  // The query key is ['user'] to uniquely identify the query.
+  // The `retry` option is set to false to prevent retries.
+  // The `refetchOnWindowFocus` option is set to true to automatically
+  // refetch the data when the window is focused.
   const { data, isLoading } = useQuery({
     queryKey: ['user'],
     queryFn: getSession,
