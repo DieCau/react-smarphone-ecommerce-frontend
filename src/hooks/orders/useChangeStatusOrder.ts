4a8d6d90f0bbb3b112be6e4fd2f76bc7dc83bc17
Diego Caucota <diegocaucota@gmail.com>
2025-06-26 01:13:32 -0300

    fix: hooks orders useChangeStatusOrder documentation

--- src/hooks/orders/useChangeStatusOrder.ts
+++ src/hooks/orders/useChangeStatusOrder.ts
@@ -3,6 +3,16 @@ import { updateOrderStatus } from '../../actions';
 import toast from 'react-hot-toast';
 
 export const useChangeStatusOrder = () => {
+  // This hook is used to change the status of an order.
+  // It uses the `useMutation` hook from `@tanstack/react-query` to
+  // perform the updateOrderStatus action and manage the loading state.
+  // It also uses the `useQueryClient` hook from `@tanstack/react-query` to
+  // invalidate the orders query after successful status update.
+  // It returns an object with the mutate function and the isPending state.
+  // The mutate function is used to trigger the updateOrderStatus action,
+  // and the isPending state is used to display a loading indicator.
+  // The `useQueryClient` hook is used to invalidate the orders query after
+  // successful status update.
   const queryClient = useQueryClient();
 
   const { mutate, isPending } = useMutation({
