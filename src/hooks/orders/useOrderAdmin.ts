841bb24341eeb655aa2ea22f45e093f7e24c3569
Diego Caucota <diegocaucota@gmail.com>
2025-06-27 00:44:22 -0300

    fix: hooks orders useOrderAdmin documentation

--- src/hooks/orders/useOrderAdmin.ts
+++ src/hooks/orders/useOrderAdmin.ts
@@ -2,6 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { getOrderById } from '../../actions';
 
 export const useOrderAdmin = (id: number) => {
+  // This hook is used to fetch a specific order by its ID for admin purposes.
+  // It uses the `useQuery` hook from `@tanstack/react-query` to
+  // perform the getOrderById action and manage the loading state.
+  // The query key is ['order', 'admin', id] to uniquely identify the query.
+  // The query function is getOrderById which fetches the order from the server.
+  // It returns an object containing the data and isLoading state.
   const { data, isLoading } = useQuery({
     queryKey: ['order', 'admin', id],
     queryFn: () => getOrderById(id),
