231e3ed13fd92fb1acd101d69e320dd5c3a741fb
Diego Caucota <diegocaucota@gmail.com>
2025-06-27 00:48:07 -0300

    fix: hooks products useDeleteProduct documentation

--- src/hooks/products/useDeleteProduct.ts
+++ src/hooks/products/useDeleteProduct.ts
@@ -3,6 +3,13 @@ import { deleteProduct } from '../../actions';
 import toast from 'react-hot-toast';
 
 export const useDeleteProduct = () => {
+  // This hook is used to delete a product.
+  // It uses the `useMutation` hook from `@tanstack/react-query` to
+  // perform the deleteProduct action and manage the loading state.
+  // The queryClient is used to invalidate the 'products' query after a successful deletion.
+  // The `mutate` function is used to trigger the deletion, and `isPending` indicates if the mutation is in progress.
+  // On success, it invalidates the 'products query to refresh the product list.
+  // On error, it logs the error and shows a toast notification.  
   const queryClient = useQueryClient();
 
   const { mutate, isPending } = useMutation({
