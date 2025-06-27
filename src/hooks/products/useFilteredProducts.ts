427f9aebc68cf3705789205d82edac7169738696
Diego Caucota <diegocaucota@gmail.com>
2025-06-27 00:50:26 -0300

    fix: hooks products useFilteredProducts documentation

--- src/hooks/products/useFilteredProducts.ts
+++ src/hooks/products/useFilteredProducts.ts
@@ -8,6 +8,11 @@ export const useFilteredProducts = ({
   page: number;
   brands: string[];
 }) => {
+  // This hook is used to fetch filtered products based on the provided page and brands.
+  // It uses the `useQuery` hook from `@tanstack/react-query` to fetch the filtered products from the server.
+  // The query key is set to ['filteredProducts', page, brands] to uniquely identify this query.
+  // The query function is getFilteredProducts which fetches the products based on the current page and brands.
+  // It returns an object containing the data, isLoading state, and totalProducts count.
   const { data, isLoading } = useQuery({
     queryKey: ['filteredProducts', page, brands],
     queryFn: () => getFilteredProducts({ page, brands }),
