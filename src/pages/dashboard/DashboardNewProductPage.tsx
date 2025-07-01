8a2f29cf2bbafca343a7efbbdd266a5d4bd18600
Diego Caucota <diegocaucota@gmail.com>
2025-06-30 21:38:01 -0300

    add: pages dashboard DashboardNewProductPage

--- src/pages/dashboard/DashboardNewProductPage.tsx
+++ src/pages/dashboard/DashboardNewProductPage.tsx
@@ -1,6 +1,11 @@
 import { FormProduct } from '../../components/dashboard';
 
 export const DashboardNewProductPage = () => {
+  // This page is used to add a new product
+  // It renders the FormProduct component with a title
+  // "Agregar Producto" (Add Product)
+  // The FormProduct component handles the form logic for adding a product
+  // and is imported from the components/dashboard directory
   return (
     <div>
       <FormProduct titleForm="Agregar Producto" />
