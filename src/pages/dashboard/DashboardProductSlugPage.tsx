42a4d4872e2ec259d26cfaf3b0c266789a9a0540
Diego Caucota <diegocaucota@gmail.com>
2025-06-30 23:37:24 -0300

    add: pages dashboard DashboardProductSlugPage

--- src/pages/dashboard/DashboardProductSlugPage.tsx
+++ src/pages/dashboard/DashboardProductSlugPage.tsx
@@ -1,6 +1,12 @@
 import { FormProduct } from '../../components/dashboard';
 
 export const DashboardProductSlugPage = () => {
+  // This component is used to edit a product in the admin dashboard.
+  // It renders a form for editing the product details.
+  // The form is displayed with the title "Editar Producto".
+  // It is expected to be used in a route that provides the product ID in the URL.
+  // The component imports the FormProduct component from the 'components/dashboard' directory.
+   
   return (
     <div>
       <FormProduct titleForm="Editar Producto" />
