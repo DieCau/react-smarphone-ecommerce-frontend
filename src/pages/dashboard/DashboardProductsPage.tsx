59b2b4a7b58a39cede697b49c8630e56cec3656b
Diego Caucota <diegocaucota@gmail.com>
2025-06-30 23:39:59 -0300

    add: pages dashboard DashboardProductsPage

--- src/pages/dashboard/DashboardProductsPage.tsx
+++ src/pages/dashboard/DashboardProductsPage.tsx
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import { TableProduct } from '../../components/dashboard';
 
 export const DashboardProductsPage = () => {
+  // This component renders the products page in the admin dashboard.
+  // It includes a link to create a new product and displays a table of existing products.
+  // The link uses an icon and is styled to appear as a button.
+  // The TableProduct component is used to display the list of products in a table format.
+  // The component is expected to be used in a route that displays the products in the admin dashboard.
+  
   return (
     <div className="h-full flex flex-col gap-2">
       <Link
