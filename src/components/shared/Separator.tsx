a6412309c13943f8e9adc34210a2600f84dbc22d
Diego Caucota <diegocaucota@gmail.com>
2025-07-02 11:42:11 -0300

    add: components shared Separator documentation

--- src/components/shared/Separator.tsx
+++ src/components/shared/Separator.tsx
@@ -3,5 +3,8 @@ interface Props {
 }
 
 export const Separator = ({ className }: Props) => {
+	// Separator component to create a horizontal line
+	// It can be used to visually separate sections in the UI
+	
 	return <div className={`bg-slate-200 h-px my-5 ${className}`} />;
 };
