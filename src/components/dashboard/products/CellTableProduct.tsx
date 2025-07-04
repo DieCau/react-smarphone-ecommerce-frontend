c28fbcb019710664ce273fac76bd36aafdc52b57
Diego Caucota <diegocaucota@gmail.com>
2025-07-03 23:51:18 -0300

    add: components dashboard product CelltableProduct documentation

--- src/components/dashboard/products/CellTableProduct.tsx
+++ src/components/dashboard/products/CellTableProduct.tsx
@@ -3,6 +3,9 @@ interface Props {
 }
 
 export const CellTableProduct = ({ content }: Props) => {
+	// This component renders a cell in a product table.
+	// It displays the content passed as a prop.
+	// The content is displayed with specific styling for better readability.
 	return (
 		<td className='p-4 font-medium tracking-tighter'>{content}</td>
 	);
