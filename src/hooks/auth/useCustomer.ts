13f96e388c87e0492d962089b95c41a7bda8994e
Diego Caucota <diegocaucota@gmail.com>
2025-07-10 17:45:45 -0300

    fix: hooks auth useCustomer documentation

--- src/hooks/auth/useCustomer.ts
+++ src/hooks/auth/useCustomer.ts
@@ -1,16 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import { getUserData } from '../../actions';
 
-export const useCustomer = (userId: string) => {
+// Obtener datos del usuario según el ID de usuario proporcionado
+// Este hook devolverá los datos del usuario y el estado de carga.
+// Solo se ejecutará si el ID de usuario es verdadero.
+// y no se reintentará en caso de error.
+// Volverá a obtener los datos cuando la ventana tenga el foco.
+// La clave de consulta es ['customer', userId] para identificar la consulta de forma única.
+// La función de consulta es getUserData, que obtiene los datos del usuario del servidor.
+// El valor de retorno es un objeto que contiene los datos y el estado isLoading.
+// El estado isLoading se establecerá en true mientras se obtengan los datos del servidor.
 
-	// Fetch user data based on the provided userId
-	// This hook will return the user data and loading state
-	// It will only run if userId is truthy
-	// and will not retry on failure.
-	// It will refetch data when the window is focused.
-	// The query key is ['customer', userId] to uniquely identify the query.
-	// The query function is getUserData which fetches the user data from the server.
-	// The return value is an object containing the data and isLoading state.
+export const useCustomer = (userId: string) => {
 	const { data, isLoading } = useQuery({
 		queryKey: ['customer', userId],
 		queryFn: () => getUserData(userId),
