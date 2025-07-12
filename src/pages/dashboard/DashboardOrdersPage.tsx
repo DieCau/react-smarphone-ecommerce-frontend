ffc20d0ce6ad918e3093dba20eea0140852dcf5d
Diego Caucota <diegocaucota@gmail.com>
2025-07-11 23:35:09 -0300

    fix: components pages dashboard DashboardOrdersPage documentation

--- src/pages/dashboard/DashboardOrdersPage.tsx
+++ src/pages/dashboard/DashboardOrdersPage.tsx
@@ -2,11 +2,10 @@ import { TableOrdersAdmin } from '../../components/dashboard';
 import { Loader } from '../../components/shared/Loader';
 import { useAllOrders } from '../../hooks';
 
+// Este componente muestra una lista de todos los pedidos en el panel de administración.
+// Obtiene los datos de los pedidos mediante un custom hook y los muestra en formato de tabla.
+// Mediante el componente TableOrdersAdmin, también muestra un indicador de carga mientras se obtienen los datos.
 export const DashboardOrdersPage = () => {
-  // This component displays a list of all orders in the admin dashboard.
-  // It fetches the order data using a custom hook and displays it in a table format
-  // using the TableOrdersAdmin component. It also shows a loading indicator while the data is
-  // being fetched.
   const { data, isLoading } = useAllOrders();
 
   if (isLoading || !data) return <Loader />;
