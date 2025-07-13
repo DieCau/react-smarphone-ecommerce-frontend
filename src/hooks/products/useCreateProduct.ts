f4fc7e622371f41602dd6032c952900a207076f8
Diego Caucota <diegocaucota@gmail.com>
2025-07-12 22:45:07 -0300

    add: hook products useCreateProduct documentation

--- src/hooks/products/useCreateProduct.ts
+++ src/hooks/products/useCreateProduct.ts
@@ -3,13 +3,13 @@ import { createProduct } from '../../actions';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
+// Este hook se usa para crear un nuevo producto.
+// Utiliza el hook `useMutation` de `@tanstack/react-query` para ejecutar 
+// la acción `createProduct` y gestionar el estado de carga.
+// La función `mutate`, que envía los datos del producto al servidor, es `createProduct`.
+// Devuelve un objeto que contiene la función `mutate` y el estado `isPending`.
+// La función `mutate` se usa para activar la acción `createProduct` al llamarla.
 export const useCreateProduct = () => {
-  // This hook is used to create a new product.
-  // It uses the `useMutation` hook from `@tanstack/react-query` to
-  // perform the createProduct action and manage the loading state.
-  // The mutation function is createProduct which sends the product data to the server.
-  // It returns an object containing the mutate function and isPending state.   
-  // The mutate function is used to trigger the createProduct action when called.
   const queryClient = useQueryClient();
   const navigate = useNavigate();
 
