75db96ebd551354ff34b0bbdf61e46d668755f4d
Diego Caucota <diegocaucota@gmail.com>
2025-07-12 22:42:31 -0300

    add: main documentation

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,11 @@ import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient();
 
+// Importar los módulos y componentes necesarios para renderizar la aplicación
+// Crear un elemento root para el contenedor de la aplicación y renderizar 
+// la aplicación usando el modo estricto de React para mejores prácticas de desarrollo
+// La aplicación usa React Router para el enrutamiento y TanStack Query para la obtención de datos
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
