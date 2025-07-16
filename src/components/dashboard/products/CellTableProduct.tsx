23a30fb85617d605aa7ee28fab5cecf9e8be0548
Diego Caucota <diegocaucota@gmail.com>
2025-07-16 00:42:59 -0300

    fix: components dashboard products CellTableProduct documentation

--- src/components/dashboard/products/CellTableProduct.tsx
+++ src/components/dashboard/products/CellTableProduct.tsx
@@ -2,10 +2,10 @@ interface Props {
 	content: string;
 }
 
+// Este componente representa una celda en una tabla de productos.
+// Muestra el contenido pasado como propiedad.
+// El contenido se muestra con un estilo específico para una mejor legibilidad.
 export const CellTableProduct = ({ content }: Props) => {
-	// This component renders a cell in a product table.
-	// It displays the content passed as a prop.
-	// The content is displayed with specific styling for better readability.
 	return (
 		<td className='p-4 font-medium tracking-tighter'>{content}</td>
 	);
